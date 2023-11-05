31766e13ac0955211f968d2fa06c3de92bebc730
Taranpreetkaur14 <148817946+Taranpreetkaur14@users.noreply.github.com>
2023-11-04 20:38:32 -0400

    Update month.py

--- hw_files/month.py
+++ hw_files/month.py
@@ -3,21 +3,37 @@
 # A program that uses functions to print out months.
 
 def monthString(monthNum):
-     """
-     Takes as input a number, monthNum, and
-     returns the corresponding month name as a string.
-     Example: monthString(1) returns "January".
-     Assumes that input is an integer ranging from 1 to 12
-     """
      
      monthString = ""
-
-     ###################################
-     ### FILL IN YOUR CODE HERE      ###
-     ### Other than your name above, ###
-     ### this is the only section    ###
-     ### you change in this program. ###
-     ###################################
+     if monthNum == 1:
+          monthString = "January"
+     elif monthNum == 2:
+          monthString = "February"
+     elif monthNum == 3:
+          monthString = "March"
+     elif monthNum == 4:
+          monthString = "April"
+     elif monthNum == 5:
+          monthString = "May"
+     elif monthNum == 6:
+          monthString = "June"
+     elif monthNum == 7:
+          monthString = "July"
+     elif monthNum == 8:
+          monthString = "August"
+     elif monthNum == 9:
+          monthString = "September"
+     elif monthNum == 10:
+          monthString = "October"
+     elif monthNum == 11:
+          monthString = "Novemeber"
+     elif monthNum == 12:
+          monthString = "December"
+     elif monthNum > 12 or monthNum ==0:
+          monthString = "invalid month number"
+return(monthString)
+          
+     
 
      return(monthString)
 
