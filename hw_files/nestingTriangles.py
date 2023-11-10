a595f4de8f85215b6373428098c1a59fa29a21df
Taranpreetkaur14 <148817946+Taranpreetkaur14@users.noreply.github.com>
2023-11-10 16:40:31 -0500

    Update nestingTriangles.py

--- hw_files/nestingTriangles.py
+++ hw_files/nestingTriangles.py
@@ -1,14 +1,10 @@
-#Name:
-#Date:
+#Name: Taranpreet Kaur
+#Date: November 10, 2023
 #A template for a program that draws nested triangles 
 
 
 import turtle
 def setUp(t, dist, col):
-    """
-    Takes three parameters, a turtle, t, the distance, dist, 
-    to move the turtle and a color, col, to set the turtle's color.
-    """
     t.penup()
     t.forward(dist)
     t.pendown()
@@ -16,36 +12,20 @@ def setUp(t, dist, col):
 
 
 def triangle(t, length):
-    """
-    Takes two parameters: a turtle and a length.
-    The function does the following: if the length is greater than 10,
-    it repeats 3 times:  moves forward that length, turns 120 degrees, 
-    and calls triangle(t, length/2).
-    """
+    if length > 10:
+        for _ in range(3):
+            t.forward(length)
+            t.left(120)
+            triangle(t, length / 2)
+            
     
-     ###################################
-     ### FILL IN YOUR CODE HERE      ###
-     ### Other than your name above, ###
-     ### these are the only sections ###
-     ### you change in this program. ###
-     ###################################    
-
-
 def nestedTriangle(t, length):
-    """
-    Takes two parameters: a turtle and a length.
-    The function does the following: if the length is greater than 10,
-    it repeats 3 times:  moves forward that length, turns 120 degrees, 
-    and calls triangle(t, length/2).
-    """
-
-     ###################################
-     ### FILL IN YOUR CODE HERE      ###
-     ### Other than your name above, ###
-     ### these are the only sections ###
-     ### you change in this program. ###
-     ###################################       
-
+     if length > 10:
+        for _ in range(3):
+            t.forward(length)
+            t.left(120)
+            nestedTriangle(t, length / 2)
+  
 
 def main():
     n = int(input('Enter length: '))
